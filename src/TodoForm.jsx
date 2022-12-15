ddb9f42c5805f2346c33e31a4955e91f4a9f795e
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-15 17:50:24 +0700

    todo 1

---
+++ src/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TodoForm = () => {
+  return (
+    <div>
+      <div>
+        <input />
+        <button>Add Todo</button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoForm;
