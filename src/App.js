81127917d673459feb58e134ce70a8ae35f7e110
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-15 11:31:54 +0700

    fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const API_URL =
+    'https://api.themoviedb.org/3/movie/popular?api_key=fa1192549721df01a1fb28a7788e6608';
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">Hello World</header>
+      <div className="search_nav">
+        <div>
+          <h1>Movies</h1>
+        </div>
+
+        <div>
+          <form>
+            <input />
+            <button>Search</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="movies"></div>
     </div>
   );
 }
