6131b1651a48e58be9d4e6513ab39fcac16da8ea
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-15 16:35:45 +0700

    restart

--- src/App.js
+++ src/App.js
@@ -1,53 +1,23 @@
-import { useEffect, useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import MovieCard from './MovieCard';
 
 function App() {
-  const API_URL =
-    'https://api.themoviedb.org/3/movie/popular?api_key=fa1192549721df01a1fb28a7788e6608';
-
-  const API_SEARCH =
-    'https://api.themoviedb.org/3/search/movie?api_key=fa1192549721df01a1fb28a7788e6608&query=';
-
-  const [movies, setMovies] = useState([]);
-  const [term, setTerm] = useState('');
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.results));
-  }, []);
-
-  console.log(movies);
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-
-    fetch(API_SEARCH + term)
-      .then((res) => res.json())
-      .then((data) => setMovies(data.results));
-  };
-
   return (
     <div className="App">
-      <div className="search_nav">
-        <div className="title">
-          <h1>Movies</h1>
-        </div>
-
-        <div className="search_box">
-          <form onSubmit={handleSearch}>
-            <input onChange={(e) => setTerm(e.target.value)} />
-            <button>Search</button>
-          </form>
-        </div>
-      </div>
-
-      <div className="movies">
-        {movies.map((movie) => (
-          <MovieCard {...movie} />
-        ))}
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
