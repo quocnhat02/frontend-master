2bc72cae2ddada64ce17d4e75a122b285d8f151b
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-15 21:08:13 +0700

    fullstack 3

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,12 +3,47 @@ import { useState } from 'react';
 
 const Register = () => {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    await fetch('http://localhost:1337/api/register', {
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+  };
 
   return (
     <div>
       <h1>Register</h1>
-      <form>
-        <input placeholder="Name" type="text" />
+      <form onSubmit={handleRegister}>
+        <input
+          placeholder="Name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder="Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <input
+          placeholder="Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <input type="submit" />
       </form>
     </div>
   );
