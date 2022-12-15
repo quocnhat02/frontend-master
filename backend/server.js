4ac236c26d5538cbd744f3f0b348e935b245ef32
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-15 21:23:28 +0700

    fullstack 6

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const model = require('./user.model');
+const { default: mongoose } = require('mongoose');
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/api/register', (req, res) => {
-  console.log(req.body.name);
+mongoose.connect('mongodb://localhost:27017/usercollection');
+
+app.post('/api/register', async (req, res) => {
+  try {
+    const user = await model.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen('1337', () => {
