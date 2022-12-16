f668c439663f65e663d71516a60b3610bbc92dcc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-12-16 08:28:35 +0700

    fullstack 9

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const model = require('./user.model');
+const bcrypt = require('bcrypt');
 
 app.use(cors());
 app.use(express.json());
@@ -11,11 +12,12 @@ mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/usercollection');
 
 app.post('/api/register', async (req, res) => {
+  const newPassword = await bcrypt.hash(req.body.password, 10);
   try {
     const user = await model.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: newPassword,
     });
 
     res.json({ status: 'ok' });
